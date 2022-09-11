8bb1897c066aee3ba1bb6ce2ae6b454133eaab9a
SosaRaul <tecraulsosa@gmail.com>
2022-09-11 02:55:09 -0300

    Solved staircase function

---
+++ Staircase/src/Staircase.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+void staircase(int);
+
+int main(void){
+
+	staircase(7);
+
+	return 0;
+}
+
+
+void staircase(int n) {
+
+	int i = 0;
+
+	// Este bucle while controla que se escriban 4 lineas de algo.
+	while(i<n){
+		// Este bucle controla la cantidad de espacios por línea.Hay n-(i+1) espacios por línea
+		for(int j = 0;j<n-(i+1);j++){
+			cout << "x";
+		}
+		// Este bucle controla la cantidad de # por línea. Hay i+1 # por línea.
+		for(int k = 0 ; k<i+1;k++){
+			cout << "#";
+		}
+		cout << endl;
+		i++;
+	}
+
+}

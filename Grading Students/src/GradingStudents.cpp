824ea1c6f25f2655c713ee3912bb786f3b7c8ab6
SosaRaul <tecraulsosa@gmail.com>
2022-09-15 05:54:15 -0300

    GradingStudents Solved

---
+++ Grading Students/src/GradingStudents.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> gradingStudents(vector<int>);
+void PrintArray(vector<int>);
+int main(void){
+
+	vector<int> grades {10,45,78,90,51};
+	cout << "Grades before rounding : ";
+    PrintArray(grades);
+	cout << "\nGrades after rounding  : ";
+    PrintArray(gradingStudents(grades));
+	return 0;
+
+}
+vector<int> gradingStudents(vector<int> grades){
+
+    for(long unsigned int grade = 0;grade < grades.size();grade ++){
+        if(grades[grade] >= 38){
+            int nextMultiple = (grades[grade]/5 +1)*5;
+            if((-grades[grade] + nextMultiple)<3){
+                grades[grade] = nextMultiple;
+            }
+        }
+    }
+    return grades;
+}
+void PrintArray(vector<int> arr){
+	for(long unsigned int index = 0;index < arr.size();index++){
+		cout << arr[index] << " ";
+	}
+}
